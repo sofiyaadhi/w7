3edd939814b552e85865fa92d40dc4f889166801
sofiya <adhisofiya@gmail.com>
2025-12-24 17:34:42 +0545

    bankaccount

---
+++ w7/BankAccount.java
@@ -0,0 +1,57 @@
+package w7;
+
+
+
+public class BankAccount
+{
+     String accountNumber;
+    String holderName;
+    double balance;
+    
+     
+    public BankAccount(String accountNumber, String holderName, double initialBalance)
+    {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = initialBalance;
+    }    
+    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited " + amount + " to account " + accountNumber);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew " + amount + " from account " + accountNumber);
+        } else if (amount > balance) {
+            System.out.println("Insufficient balance in account " + accountNumber);
+        } else {
+            System.out.println("Invalid withdrawal amount!");
+        }
+    }
+    
+    public void displayBalance() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Holder Name: " + holderName);
+        System.out.println("Current Balance: " + balance);
+    }
+    
+     public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public String getHolderName() {
+        return holderName;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+
+}
---
+++ w7/main.java
@@ -0,0 +1,31 @@
+package w7;
+
+public class main
+{
+    
+     public static void main(String[] args) {
+        System.out.println("Bank Account Simulation ");
+        BankAccount account1 = new BankAccount("ACC001", "John Smith", 1000.0);
+        BankAccount account2 = new BankAccount("ACC002", "Sarah Johnson", 500.0);
+        System.out.println("Initial Account Details:");
+        account1.displayBalance();
+        account2.displayBalance();
+        
+        System.out.println(" Performing Transactions ");
+        
+        System.out.println("Transactions for " + account1.getHolderName() + ":");
+        account1.deposit(500.0);
+        account1.withdraw(200.0);
+        
+        System.out.println();
+        System.out.println("Transactions for " + account2.getHolderName() + ":");
+        account2.deposit(1000.0);
+        account2.withdraw(300.0);
+        account2.withdraw(2000.0); 
+        System.out.println();
+        
+        System.out.println(" Final Account Details ");
+        account1.displayBalance();
+        account2.displayBalance();
+    }
+}
